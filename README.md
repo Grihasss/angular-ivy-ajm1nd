92d0f2400bf15b86c238a2b565f06794f2cf0520
Grihasss <grihas56@mail.ru>
2021-04-02 07:06:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ajm1nd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ajm1nd)
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { SummaryComponent } from './summary/summary.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, 
+  RouterModule.forRoot([
+      { path: '', component: SummaryComponent },
+      { path: 'products/:productId', component: NavigatorComponent },
+    ])
+  ],
+  declarations: [ AppComponent, HelloComponent, NavigatorComponent, SummaryComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
